f51258444e15760910a194c1fe99f7e450ffe723
Clément DOUIN <clement.douin@posteo.net>
2025-10-27 09:10:10 +0100

    docs: fix wrong AGPL license

--- package.nix
+++ package.nix
@@ -73,7 +73,7 @@ rustPlatform.buildRustPackage rec {
     mainProgram = "calendula";
     homepage = "https://github.com/pimalaya/calendula";
     changelog = "https://github.com/pimalaya/calendula/blob/v${version}/CHANGELOG.md";
-    license = lib.licenses.agpl3Only;
+    license = lib.licenses.agpl3Plus;
     maintainers = with lib.maintainers; [
       soywod
     ];
