aac469a864b8370bbe4452e6953b2fd640828074
Clément DOUIN <clement.douin@posteo.net>
2026-06-16 02:08:31 +0200

    nix: fix build

--- package.nix
+++ package.nix
@@ -1,81 +1,78 @@
-# TODO: move this to nixpkgs
-# This file aims to be a replacement for the nixpkgs derivation.
-
 {
+  buildFeatures ? [ ],
+  buildNoDefaultFeatures ? false,
+  buildPackages,
+  fetchFromGitHub,
+  installManPages ? stdenv.buildPlatform.canExecute stdenv.hostPlatform,
+  installShellCompletions ? stdenv.buildPlatform.canExecute stdenv.hostPlatform,
+  installShellFiles,
   lib,
+  openssl,
   pkg-config,
   rustPlatform,
-  fetchFromGitHub,
   stdenv,
-  apple-sdk,
-  installShellFiles,
-  installShellCompletions ? stdenv.buildPlatform.canExecute stdenv.hostPlatform,
-  installManPages ? stdenv.buildPlatform.canExecute stdenv.hostPlatform,
-  withNoDefaultFeatures ? false,
-  withFeatures ? [ ],
 }:
 
 let
-  version = "0.1.0";
-  hash = "";
-  cargoHash = "";
-in
+  version = "0.2.0";
+  emul = stdenv.hostPlatform.emulator buildPackages;
+  exe = stdenv.hostPlatform.extensions.executable;
 
-rustPlatform.buildRustPackage rec {
-  inherit cargoHash version;
+in
+rustPlatform.buildRustPackage {
+  inherit version buildNoDefaultFeatures buildFeatures;
 
   pname = "calendula";
+  cargoHash = "";
 
   src = fetchFromGitHub {
-    inherit hash;
+    hash = "";
     owner = "pimalaya";
     repo = "calendula";
     rev = "v${version}";
   };
 
-  buildNoDefaultFeatures = withNoDefaultFeatures;
-  buildFeatures = withFeatures;
+  env.OPENSSL_NO_VENDOR = true;
 
   nativeBuildInputs = [
     pkg-config
-  ]
-  ++ lib.optional (installManPages || installShellCompletions) installShellFiles;
+    installShellFiles
+  ];
 
-  buildInputs = lib.optional stdenv.hostPlatform.isDarwin apple-sdk;
+  buildInputs = lib.optional (builtins.elem "native-tls" buildFeatures) openssl;
 
-  # unit tests only
-  cargoTestFlags = [ "--lib" ];
+  # most of the tests are lib side
   doCheck = false;
-  auditable = false;
 
-  postInstall = ''
-    mkdir -p $out/share/{completions,man}
-  ''
-  + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-    "$out"/bin/calendula man "$out"/share/man
-  ''
-  + lib.optionalString installManPages ''
-    installManPage "$out"/share/man/*
-  ''
-  + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-    "$out"/bin/calendula completion bash > "$out"/share/completions/calendula.bash
-    "$out"/bin/calendula completion elvish > "$out"/share/completions/calendula.elvish
-    "$out"/bin/calendula completion fish > "$out"/share/completions/calendula.fish
-    "$out"/bin/calendula completion powershell > "$out"/share/completions/calendula.powershell
-    "$out"/bin/calendula completion zsh > "$out"/share/completions/calendula.zsh
-  ''
-  + lib.optionalString installShellCompletions ''
-    installShellCompletion "$out"/share/completions/calendula.{bash,fish,zsh}
-  '';
+  postInstall =
+    lib.optionalString (lib.hasInfix "wine" emul) ''
+      export WINEPREFIX="''${WINEPREFIX:-$(mktemp -d)}"
+      mkdir -p $WINEPREFIX
+    ''
+    + ''
+      mkdir -p $out/share/{applications,completions,man}
+      ${emul} "$out"/bin/calendula${exe} manuals "$out"/share/man
+      ${emul} "$out"/bin/calendula${exe} completions -d "$out"/share/completions bash elvish fish powershell zsh
+    ''
+    + lib.optionalString installManPages ''
+      installManPage "$out"/share/man/*
+    ''
+    + lib.optionalString installShellCompletions ''
+      installShellCompletion --cmd calendula \
+        --bash "$out"/share/completions/calendula.bash \
+        --fish "$out"/share/completions/calendula.fish \
+        --zsh "$out"/share/completions/_calendula
+    '';
 
   meta = {
     description = "CLI to manage calendars";
     mainProgram = "calendula";
     homepage = "https://github.com/pimalaya/calendula";
-    changelog = "https://github.com/pimalaya/calendula/blob/v${version}/CHANGELOG.md";
-    license = with lib.licenses; [ mit asl20 ];
-    maintainers = with lib.maintainers; [
-      soywod
+    changelog = "https://github.com/pimalaya/calendula/blob/master/CHANGELOG.md";
+    license = with lib.licenses; [
+      mit
+      asl20
     ];
+    maintainers = with lib.maintainers; [ soywod ];
   };
 }
